14e27dca6a0d007cc0ab134bc3182cbb78ebfe6d
ethan <ethanmannette@gmail.com>
2015-08-27 10:27:54 -0600

    red box appears in bottom right corner on double click

--- canvas.js
+++ canvas.js
@@ -2,17 +2,20 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var size = { x: canvas.width, y: canvas.height };
 
+// draws border
 function drawBorder () {
   context.strokeRect(0, 0, size.x, size.y);
 }
 drawBorder();
 
+// adds blue box in top left on single click
 canvas.onclick = function () {
   context.fillStyle = 'blue';
   context.fillRect(0, 0, (size.x / 4), (size.y / 4));
 };
 
+// adds red box in bottom right corner
 canvas.ondblclick = function () {
   context.fillStyle = 'red';
-  context.fillRect((size.x / 2), (size.y / 2), (size.x / 4), (size.y / 4));
+  context.fillRect((size.x / 1.34), (size.y / 1.34), (size.x / 4), (size.y / 4));
 };
