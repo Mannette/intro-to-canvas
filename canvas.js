2c2913aa0d3469f0a237f5b867a63c11b1a7a1b5
ethan <ethanmannette@gmail.com>
2015-08-27 10:23:50 -0600

    added functions for single and double click

--- canvas.js
+++ canvas.js
@@ -1,6 +1,18 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var size = { x: canvas.width, y: canvas.height };
+
+function drawBorder () {
+  context.strokeRect(0, 0, size.x, size.y);
+}
+drawBorder();
+
+canvas.onclick = function () {
+  context.fillStyle = 'blue';
+  context.fillRect(0, 0, (size.x / 4), (size.y / 4));
+};
+
+canvas.ondblclick = function () {
+  context.fillStyle = 'red';
+  context.fillRect((size.x / 2), (size.y / 2), (size.x / 4), (size.y / 4));
+};
