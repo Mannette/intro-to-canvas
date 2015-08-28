332a0c9957eba5c17bff042e51170952f5950c5e
ethan <ethanmannette@gmail.com>
2015-08-28 11:18:23 -0600

    Added key functions for keys: t, r, c, d. D key produces diamond, needs refactoring to make true diamond.

--- canvas.js
+++ canvas.js
@@ -8,6 +8,22 @@ function drawBorder () {
 }
 drawBorder();
 
+// creates rectangles
+function rectangles () {
+  randomX = Math.floor(Math.random() * size.x);
+  randomY = Math.floor(Math.random() * size.y);
+  randomWidth = Math.floor(Math.random() * (size.x / 2));
+  randomHeight = Math.floor(Math.random() * (size.y / 2));
+
+  // for (var i = 1; i > 0; i -= 0.1) {
+  //   console.log(i);
+  //   return i;
+  // }
+
+  context.fillStyle = getRandomColor();
+  context.fillRect(randomX, randomY, randomWidth, randomHeight);
+}
+
 // creates circles
 function circles () {
   var randomCenterX = Math.floor(Math.random() * size.x);
@@ -115,17 +131,100 @@ randomButton.onclick = function () {
   randomWidth = Math.floor(Math.random() * (size.x / 2));
   randomHeight = Math.floor(Math.random() * (size.y / 2));
 
-  context.fillStyle = getRandomColor();
+  // context.fillStyle = getRandomColor();
   if (shape === 1) {
     circles();
   } else {
-    context.fillRect(randomX, randomY, randomWidth, randomHeight);
+    rectangles();
   }
 };
 
 clearButton.onclick = function () {
   context.clearRect(0, 0, size.x, size.y);
   drawBorder();
+};
+
+// r keycode = 82
+// c keycode = 67
+// t keycode = 84
+// d keycode = 68
+document.onkeydown = function (e) {
+  e = e || window.event;
+  switch (e.which || e.keyCode) {
+
+    // draws rectangle
+    case 82:
+      randomX = Math.floor(Math.random() * size.x);
+      randomY = Math.floor(Math.random() * size.y);
+      randomWidth = Math.floor(Math.random() * (size.x / 2));
+      randomHeight = Math.floor(Math.random() * (size.y / 2));
+
+      context.fillStyle = getRandomColor();
+      context.fillRect(randomX, randomY, randomWidth, randomHeight);
+      break;
+
+    // draws circle
+    case 67:
+      var randomCenterX = Math.floor(Math.random() * size.x);
+      var randomCenterY = Math.floor(Math.random() * size.y);
+      var randomSize = Math.floor(Math.random() * 100);
+
+      context.beginPath();
+      context.arc(
+        randomCenterX,
+        randomCenterY,
+        randomSize,
+        0,
+        2 * Math.PI
+      );
+      context.fillStyle = getRandomColor();
+      context.fill();
+      break;
+
+    // draws triangle
+    case 84:
+      randomX = Math.floor(Math.random() * size.x);
+      randomY = Math.floor(Math.random() * size.y);
+      var firstVrtxX = Math.floor(Math.random() * size.x);
+      var firstVrtxY = Math.floor(Math.random() * size.y);
+      var secondVrtxX = Math.floor(Math.random() * size.x);
+      var secondVrtxY = Math.floor(Math.random() * size.y);
+
+      context.beginPath();
+      context.moveTo(randomX, randomY);
+      context.lineTo(firstVrtxX, firstVrtxY);
+      context.lineTo(secondVrtxX, secondVrtxY);
+      context.closePath();
+
+      context.fillStyle = getRandomColor();
+      context.fill();
+      break;
+
+    // draws diamond
+    case 68:
+      randomX = Math.floor(Math.random() * size.x);
+      randomY = Math.floor(Math.random() * size.y);
+      var firstVrtxX = Math.floor(Math.random() * size.x);
+      var firstVrtxY = Math.floor(Math.random() * size.y);
+      var secondVrtxX = Math.floor(Math.random() * size.x);
+      var secondVrtxY = Math.floor(Math.random() * size.y);
+      var thirdVrtxX = Math.floor(Math.random() * size.x);
+      var thirdVrtxY = Math.floor(Math.random() * size.y);
+
+      context.beginPath();
+      context.moveTo(randomX, randomY);
+      context.lineTo(firstVrtxX, firstVrtxY);
+      context.lineTo(secondVrtxX, secondVrtxY);
+      context.lineTo(thirdVrtxX, thirdVrtxY);
+      context.closePath();
+
+      context.fillStyle = getRandomColor();
+      context.fill();
+      break;
+
+    default: return;
+  }
+  e.preventDefault();
 };
 // ---------------------- //
 // ** END CLICK EVENTS ** //
